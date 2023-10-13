91e8205216483129a4d791a4ec2822748e9181ed
JJJae0 <jaeyeong345@naver.com>
2023-10-13 09:35:58 +0900

    기본 게시글 UI생성

---
+++ src/components/sub/Community/Community.jsx
@@ -0,0 +1,37 @@
+import Layout from '../../common/layout/Layout';
+import './Community.scss';
+import { useRef, useState } from 'react';
+
+export default function Community() {
+	const refInput = useRef(null);
+	const refTextarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+	return (
+		<Layout title={'Communtiy'}>
+			<div className='inputBox'>
+				<input ref={refInput} type='text' placeholder='제목을 입력하세요.' />
+				<br />
+				<textarea
+					ref={refTextarea}
+					cols='30'
+					rows='3'
+					placeholder='본문을 입력하세요.'
+				></textarea>
+
+				<nav className='btnSet'>
+					<button>cancle</button>
+					<button>write</button>
+				</nav>
+			</div>
+		</Layout>
+	);
+}
+
+/*
+Create : 게시글 저장
+Read : 게시글 보기
+Update : 게시글 수정
+Delete : 게시글 삭제
+
+localStorege : 모든 브라우저가 가지고 있는 경량의 저장소 (문자열: 5MB)
+*/
